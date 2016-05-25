bb8b935c2b1e7dc3ec2c7f963879981dbd96b28d
Michael Gutteridge <mrg@fhcrc.org>
2016-05-25 14:07:06 -0700

    first working version

--- recipes/upload.rb
+++ recipes/upload.rb
@@ -4,28 +4,13 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-#execute "list_cookbooks" do
-#    command "knife supermarket show congenial-happiness 0.1.33 --config /var/spool/btb/.chef/knife.rb --verbose"
-#    live_stream true
-#    user 'btb'
-#end
-#execute "list_cookbooks" do
-#    command "knife supermarket show congenial-happiness 0.1.33 #{knife_options}"
-#    live_stream true
-#    user node['kwik-e-mart']['user']['username']
-#end
-#execute "whoami" do
-#    command "whoami"
-#    live_stream true
-#    user 'btb'
-#end
-
 # This cookbook goes through all of the cookbooks in the configured
 # source directory- these should be cookbooks delivered from
 # upstream (i.e. CircleCI)
 #
 # For each cookbook, upload first to the supermarket, then to the server
 # using knife
+
 knife_options = "--config #{node['kwik-e-mart']['user']['unix_home']}/.chef/knife.rb --verbose"
 upload_dir = node['kwik-e-mart']['upload_dir']
 
@@ -34,33 +19,61 @@ directory upload_dir do
     recursive true
 end
 
-Dir.glob( "#{upload_dir}/*.tar.gz" ).each do |archive|
-    workdir= "#{upload_dir}/#{::File.basename(archive, '.tar.gz')}"
-    log 'archive extraction' do
-        level :warn
-        message "Extracting #{archive} into #{workdir}"
-    end
-    directory workdir do
-        owner node['kwik-e-mart']['user']['username']
-        recursive true
-    end
-    bash 'extract cookbook' do
-        cwd workdir
-        code "tar xvf #{archive} --strip=1"
-    end
-    # Read metatdata for cookbook name
-    md_read = Chef::Cookbook::Metadata.new()
-    cookbook_md = md_read.from_file("#{workdir}/metadata.rb")
-    cookbook_name = md_read.name
-    log 'a man needs a name' do
-        level :warn
-        message "cookbook name is #{cookbook_name}"
-    end
-    execute 'upload to supermarket' do
-        command "knife supermarket share #{cookbook_name} -o .. #{knife_options}"
-        user node['kwik-e-mart']['user']['username']
-        cwd workdir
-        live_stream true
+workdirs = Array.new
+
+ruby_block "atend" do
+    action :nothing
+    block do
+        workdirs.each do |dir|
+            Chef::Log.warn( "dir is #{dir}" )
+            md_read = Chef::Cookbook::Metadata.new()
+            cookbook_md = md_read.from_file("#{dir}/metadata.rb")
+            cookbook_name = md_read.name
+            Chef::Log.warn( "cookbook name is #{cookbook_name}" )
+            cmd = ::Mixlib::ShellOut.new(
+              "knife supermarket share #{cookbook_name} -o .. #{knife_options}",
+              :user => node['kwik-e-mart']['user']['username'],
+              :cwd => dir
+            )
+            cmd.run_command
+            cmd.error!
+            Chef::Log.warn( "Supermarket upload OK: #{cmd.stdout}" )
+            cmd = ::Mixlib::ShellOut.new(
+              "knife cookbook upload #{cookbook_name} -o .. #{knife_options}",
+              :user => node['kwik-e-mart']['user']['username'],
+              :cwd => dir
+            )
+            cmd.run_command
+            cmd.error!
+            Chef::Log.warn( "Upload to server OK: #{cmd.stdout}" )
+        end
     end
 end
 
+
+Dir.glob( "#{upload_dir}/*.tar.gz" ).each do |archive|
+    dirname = ::File.basename(archive, '.tar.gz')
+    workdir = "#{upload_dir}/#{dirname}"
+    log "#{dirname} archive extraction" do
+        level :warn
+        message "Extracting #{archive} into #{workdir}"
+    end
+    directory "#{dirname}_create" do
+        path workdir
+        owner node['kwik-e-mart']['user']['username']
+        recursive true
+    end
+    bash "#{dirname} extract cookbook" do
+        cwd workdir
+        user node['kwik-e-mart']['user']['username']
+        code "tar xvf #{archive} --strip=1"
+        creates "#{workdir}/metadata.rb"
+    end
+    ruby_block "add_to_workdirs" do
+        block do
+            workdirs.push( workdir )
+        end
+        notifies :create, resources("ruby_block[atend]"), :delayed
+    end
+
+end
